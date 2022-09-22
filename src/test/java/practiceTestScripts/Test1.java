5b2eab77c6628b2f866b372c58fc6738e2b490b8
BHARATI <BHARATI@DESKTOP-OL5B2QD>
2022-09-22 20:58:33 +0530

    First Commit

---
+++ src/test/java/practiceTestScripts/Test1.java
@@ -0,0 +1,10 @@
+package practiceTestScripts;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+@Test
+private void TC1() {
+	System.out.println("Test Case1 inititated");
+}
+}
---
+++ src/test/java/practiceTestScripts/Test2.java
@@ -0,0 +1,8 @@
+package practiceTestScripts;
+
+public class Test2 {
+	public void TC2() {
+		System.out.println("Test Case2 initiated");
+	}
+
+}
