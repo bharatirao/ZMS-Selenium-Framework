f8769ec9ff5161013b25eaa2c48ac1c62bf520cd
BHARATI <BHARATI@DESKTOP-OL5B2QD>
2022-09-23 11:55:19 +0530

    driver updated

--- src/test/java/practiceTestScripts/Test1.java
+++ src/test/java/practiceTestScripts/Test1.java
@@ -4,16 +4,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Test1 {
 @Test
 private void TC1Test() {
-	System.out.println("Test Case1 inititated");
-	System.out.println("modified code");
-	String browser = System.getProperty("br");
-	String url = System.getProperty("ur");
-	System.out.println(browser);
-	
+//	System.out.println("Test Case1 inititated");
+//	System.out.println("modified code");
+//	String browser = System.getProperty("br");
+//	String url = System.getProperty("ur");
+//	System.out.println(browser);
+	WebDriverManager.chromedriver().setup();
 	WebDriver driver=new ChromeDriver();
+	
 	driver.manage().window().maximize();
 	driver.get("www.skillrary.com");
 	driver.close();
