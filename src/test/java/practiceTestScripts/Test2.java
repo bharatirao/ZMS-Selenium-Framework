2bc6061895cd420c80d956cd46fc871babea64c8
BHARATI <BHARATI@DESKTOP-OL5B2QD>
2022-09-23 11:45:54 +0530

    browser launching

--- src/test/java/practiceTestScripts/Test2.java
+++ src/test/java/practiceTestScripts/Test2.java
@@ -1,8 +1,15 @@
 package practiceTestScripts;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test2 {
 	public void TC2Test() {
 		System.out.println("Test Case2 initiated");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("www.skillrary.com");
+		driver.close();
 		
 	}
 
