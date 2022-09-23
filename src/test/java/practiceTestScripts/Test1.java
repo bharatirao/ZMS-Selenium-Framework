268af65bdc971090937c8337e7ac581c673f66e7
BHARATI <BHARATI@DESKTOP-OL5B2QD>
2022-09-23 11:32:49 +0530

    browser updation

--- src/test/java/practiceTestScripts/Test1.java
+++ src/test/java/practiceTestScripts/Test1.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class Test1 {
 @Test
-private void TC1() {
+private void TC1Test() {
 	System.out.println("Test Case1 inititated");
 	System.out.println("modified code");
 	String browser = System.getProperty("br");
--- src/test/java/practiceTestScripts/Test2.java
+++ src/test/java/practiceTestScripts/Test2.java
@@ -1,7 +1,7 @@
 package practiceTestScripts;
 
 public class Test2 {
-	public void TC2() {
+	public void TC2Test() {
 		System.out.println("Test Case2 initiated");
 		
 	}
